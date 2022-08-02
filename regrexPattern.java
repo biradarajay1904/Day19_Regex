5b43dd5c5a4ec7d314f48b5b60fcaee1a5cf97a1
biradarajay1904 <biradarajay04@gmail.com>
2022-08-02 20:49:12 +0530

    added: validate password having 8 charcter

--- regrexPattern.java
+++ regrexPattern.java
@@ -12,12 +12,20 @@ public class regrexPattern {
 		//System.out.println("Enter the Phone number : ");
 		//String phoneNumber = sc.nextLine();
 	//email pattern	"^[a-z](.+)@(\\S+)$"
-		System.out.println("Enter the Email address : ");
-		String Email = sc.nextLine();
-		//boolean validateLastName  = Pattern.matches("^((91){1})? ?[0-9]{1}[0-9]{9}$", phoneNumber);  
+		//System.out.println("Enter the Email address : ");
+		//String Email = sc.nextLine();
+		//boolean validateLastName  = Pattern.matches("^((91){1})? ?[0-9]{9}$", phoneNumber);  
 		//System.out.println(validateLastName );
-		boolean validateEmail = Pattern.matches("^[a-z](.+)@(\\S+)$", Email);  
-		System.out.println(validateEmail );
+		//boolean validateEmail = Pattern.matches("^[a-z](.+)@(\\S+)$", Email);  
+		//System.out.println(validateEmail );
+		System.out.println("Enter the Password : ");
+		String password = sc.nextLine();
+		boolean validatePassword  = Pattern.matches("^(?=.*[a-z]).{8,}$", password );  
+		if (validatePassword) {
+		System.out.println("password is correct ");
+	} else {
+		System.out.println("password is incorrect ");
+	}
 	}
 
 }
