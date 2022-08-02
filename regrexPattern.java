bdf56277f0619d8fb2072d4ad944a35c1b2631af
biradarajay1904 <biradarajay04@gmail.com>
2022-08-02 13:03:28 +0530

    added: validate mobile number

--- regrexPattern.java
+++ regrexPattern.java
@@ -7,12 +7,13 @@ public class regrexPattern {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Last name : ");
-		String LastName = sc.nextLine();
-	
+		//System.out.println("Enter the Last name : ");
+		//String LastName = sc.nextLine();
+		System.out.println("Enter the Phone number : ");
+		String phoneNumber = sc.nextLine();
 		
 		
-		boolean validateLastName  = Pattern.matches("^[A-Z]{1}[a-z]{2,20}$",LastName);  
+		boolean validateLastName  = Pattern.matches("((\\+*)((0[ -]*)*|((91 )*))((\\d{12})+|(\\d{10})+))|\\d{5}([- ]*)\\d{6} ", phoneNumber);  
 		System.out.println(validateLastName );
 	}
 
