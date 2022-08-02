e1e052edcbea67e17b702ce72610d27b6acabcae
biradarajay1904 <99815064+biradarajay1904@users.noreply.github.com>
2022-08-02 22:40:47 +0530

    Update regrexPattern.java

--- regrexPattern.java
+++ regrexPattern.java
@@ -7,20 +7,11 @@ public class regrexPattern {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//System.out.println("Enter the Last name : ");
-		//String LastName = sc.nextLine();
-		//System.out.println("Enter the Phone number : ");
-		//String phoneNumber = sc.nextLine();
-	//email pattern	"^[a-z](.+)@(\\S+)$"
+		
 		System.out.println("Enter the Email address : ");
 		String Email = sc.nextLine();
-		//boolean validateLastName  = Pattern.matches("^((91){1})? ?[0-9]{9}$", phoneNumber);  
-		//System.out.println(validateLastName );
 		boolean validateEmail = Pattern.matches("^[a-z](.+)@_-(\\S+)$", Email);  
-		//System.out.println(validateEmail );
-		//System.out.println("Enter the Password : ");
-		//String password = sc.nextLine();
-///boolean  validatePassword  = Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>])(?=.*[0-9]).{8,}$", password );  
+		
 		if ( validateEmail) {
 		System.out.println("password is correct ");
 	} else {
