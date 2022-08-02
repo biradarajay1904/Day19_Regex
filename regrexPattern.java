2ce79e66a8845bf77ac1c647344a1e6a08a19b20
biradarajay1904 <99815064+biradarajay1904@users.noreply.github.com>
2022-08-02 20:37:35 +0530

    Update regrexPattern.java

--- regrexPattern.java
+++ regrexPattern.java
@@ -8,12 +8,12 @@ public class regrexPattern {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the First name : ");
-		String FirstName = sc.nextLine();
+		String firstName = sc.nextLine();
 	
 		
 		
-		boolean validateFirstName  = Pattern.matches("[A-Z][a-z]{3}" , FirstName);  
-		System.out.println(validateFirstName);
+		boolean validatefirstName  = Pattern.matches("[A-Z][a-z]{3}" , FirstName);  
+		System.out.println(validatefirstName);
 	}
 
 }
