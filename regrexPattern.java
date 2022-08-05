99babe9e5920ed3f4a703bf2d79b846ba1b01b4c
biradarajay1904 <99815064+biradarajay1904@users.noreply.github.com>
2022-08-05 19:46:44 +0530

    Update regrexPattern.java

--- regrexPattern.java
+++ regrexPattern.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class regrexPattern {
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter the Email address : ");
-		String Email = sc.nextLine();
-		boolean validateEmail = Pattern.matches("^[a-z](.+)@_-(\\S+)$", Email);  
-		
-		if ( validateEmail) {
-		System.out.println("password is correct ");
-	} else {
-		System.out.println("password is incorrect ");
-	}
+		String email = sc.nextLine();
+		boolean validateEmail = Pattern.matches("^[a-z]*.[a-z0-9]+@-_[a-z]{2,3}(.[a-z]{3})*$", email);
+
+		if (validateEmail) {
+			System.out.println("Email is correct ");
+		} else {
+			System.out.println("Email is incorrect ");
+		}
 	}
 
 }
